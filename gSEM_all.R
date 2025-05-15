d2605b992a5b1e4fa7f615996c14c40c54dff6ee
anne02s <anne.l.snijder@gmail.com>
2025-05-15 08:59:19 +0200

    Update gSEM_all.R

--- gSEM_all.R
+++ gSEM_all.R
@@ -29,17 +29,14 @@ load("LDSCoutput.Rdata")
 
 
 #model all
-model <- 'F1 =~ NA*CAD + STR + HF + BrS
+model_all <- 'F1 =~ NA*CAD + STR + HF + BrS
 F2 =~ A*F1 + A*ANX
 F2 =~ 1*F2'
 
-run_model <- usermodel(LDSCoutput, estimation = "DWLS", model = model, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
-run_model
-
-
+run_model_all <- usermodel(LDSCoutput, estimation = "DWLS", model = model_all, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
 
 # other model seperate
-model <- 'F1 =~ NA*CAD + STR + HF
+model_seperate <- 'F1 =~ NA*CAD + STR + HF
 F2 =~ A*BrS + A*HF 
 F2 ~~ F1
 F3 =~ B*F1 + B*ANX
@@ -47,13 +44,7 @@ F4 =~ C*F2 + C*ANX
 F3 ~~ 1*F3
 F4 ~~ 1*F4'
 
-model <- 'F1 =~ NA*CAD + STR + HF
-F2 =~ A*BrS + A*HF 
-F2 ~~ F1
-F3 =~ B*F1 + B*ANX
-F4 =~ C*F2 + C*ANX
-F5 =~ D*F4 + D*F3
-F5 ~~ 1*F5'
+run_model_seperate <- usermodel(LDSCoutput, estimation = "DWLS", model = model_seperate, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
 
            
 ref= "reference.1000G.maf.0.005.txt"
@@ -67,11 +58,7 @@ betas=NULL
 
 
 PSYCH_sumstats <-sumstats(files=data$files,ref=ref,trait.names=data$traitnames,se.logit=se.logit,OLS=NULL,linprob=Hail,N=N,betas=NULL, info.filter=.6, maf.filter=0.01,keep.indel=FALSE,parallel=TRUE,cores=NULL)
-save(PSYCH_sumstats, file = "PSYCH_sumstats.Rdata")
-load("PSYCH_sumstats.Rdata")
-#note that the code written above specifies all arguments for completeness, but as many of these arguments
-#are set to the package default it could also be written as below and produce the same result:
-#PSYCH_factor <- commonfactorGWAS(covstruc = LDSCoutput, SNPs = PSYCH_sumstats, estimation = "DWLS", parallel=TRUE)
+
 PSYCH_factor <- userGWAS(covstruc = LDSCoutput,
                          SNPs = PSYCH_sumstats,
                          estimation = "DWLS",
@@ -82,7 +69,7 @@ PSYCH_factor <- userGWAS(covstruc = LDSCoutput,
                          SNPSE = FALSE,
                          parallel = FALSE,
                          fix_measurement=T)
-PSYCH_facto
+
 
 
 
