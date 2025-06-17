31ecb15689027eee2b830767b048fa2a9906b716
anne02s <anne.l.snijder@gmail.com>
2025-06-17 11:56:08 +0200

    Update gSEM_all.R

--- gSEM_all.R
+++ gSEM_all.R
@@ -6,17 +6,17 @@ library(tidyr)
 
 
 data <- data.frame(
-  samp.prevelance = c(0.08167744, 2820/12821, 120788/980319, 73652/1308460, 0.07981),
-  pop.prevelance = c(0.0172, 0.05, 0.063, 0.019, 0.05),
-  traitnames = c("HF", "BrS", "CAD", "STR", "ANX"),
-  trait_path = c("munge/HF.sumstats.gz", "munge/BrS.sumstats.gz", "munge/CAD.sumstats.gz", "munge/STR.sumstats.gz", "munge/ANX.sumstats.gz"),
+  samp.prevelance = c(0.08167744, 0.219, 0.123, 0.056, 0.316, 0.07981),
+  pop.prevelance = c(0.017, 0.0001, 0.036, 0.092,0.22, 0.05),
+  traitnames = c("HF", "BrS", "CAD", "STR", "HYP", "ANX"),
+  trait_path = c("munge/HF.sumstats.gz", "munge/BrS.sumstats.gz", "munge/CAD.sumstats.gz", "munge/STR.sumstats.gz", "munge/HYP_2019.sumstats", "munge/ANX.sumstats.gz"),
   files = c("clean/Henry_2025_HF_EUR_clean.txt",
             "clean/Barc_2022_BrS_clean.txt",
             "clean/aragam_2022_CAD_clean.txt",
             "clean/mishra_2022_stroke_clean.txt",
-             "clean/Friligkou_2024_ANX_EUR_clean.txt")
+            "clean/Zhu_2019_HYP_clean.txt",
+            "clean/Friligkou_2024_ANX_EUR_clean.txt")
 )
-
 
 #ldsc
 ld <- "eur_w_ld_chr/eur_w_ld_chr"
@@ -27,11 +27,9 @@ wld <- "eur_w_ld_chr/eur_w_ld_chr"
 load("LDSCoutput.Rdata")
 
 
-
 #model all
 model_all <- 'F1 =~ NA*CAD + STR + HF + BrS
-F2 =~ A*F1 + A*ANX
-F2 =~ 1*F2'
+F2 =~ A*F1 + A*ANX'
 
 run_model_all <- usermodel(LDSCoutput, estimation = "DWLS", model = model_all, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
 
@@ -40,35 +38,24 @@ model_seperate <- 'F1 =~ NA*CAD + STR + HF
 F2 =~ A*BrS + A*HF 
 F2 ~~ F1
 F3 =~ B*F1 + B*ANX
-F4 =~ C*F2 + C*ANX
-F3 ~~ 1*F3
-F4 ~~ 1*F4'
+F4 =~ C*F2 + C*ANX'
 
 run_model_seperate <- usermodel(LDSCoutput, estimation = "DWLS", model = model_seperate, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
 
-           
-ref= "reference.1000G.maf.0.005.txt"
-se.logit=c(T,T,T,T,T)
-Hail=c(F,F,F,F,F)
-info.filter=.6
-maf.filter=0.01
-OLS=NULL
-N=NULL
-betas=NULL
+# model for userGWAS, std.lv = FALSE
 
+model_all <- 'F1 =~ NA*CAD + STR + HF + BrS + HYP
+          F2 =~ 0.27054220*F1 + 0.27054220*ANX
+F1 ~~ 1*F1
+F2 ~~ 1*F2'
 
-PSYCH_sumstats <-sumstats(files=data$files,ref=ref,trait.names=data$traitnames,se.logit=se.logit,OLS=NULL,linprob=Hail,N=N,betas=NULL, info.filter=.6, maf.filter=0.01,keep.indel=FALSE,parallel=TRUE,cores=NULL)
-
-PSYCH_factor <- userGWAS(covstruc = LDSCoutput,
-                         SNPs = PSYCH_sumstats,
-                         estimation = "DWLS",
-                         model = model,
-                         printwarn = TRUE,
-                         cores = 1,
-                         toler = FALSE,
-                         SNPSE = FALSE,
-                         parallel = FALSE,
-                         fix_measurement=T)
+model_seperate <- 'F1 =~ NA*CAD + STR + HF
+F2 =~ A*BrS + A*HF 
+F2 ~~ F1
+F3 =~ 0.86047998*F1 + 0.86047998*ANX
+F4 =~ 0.93108622*F2 + 0.93108622*ANX
+F3 ~~ 1*F3
+F4 ~~ 1*F4'
 
 
 
